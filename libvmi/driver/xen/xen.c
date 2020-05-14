062cd0eeb1b253b431eafad598ac60788ab34906
root <ml@icedev.pl>
2020-05-14 20:34:57 +0000

    fix

--- libvmi/driver/xen/xen.c
+++ libvmi/driver/xen/xen.c
@@ -2748,19 +2748,33 @@ status_t
 xen_wait_vm(
     vmi_instance_t vmi)
 {
-	xen_instance_t *xen = xen_get_instance(vmi);
-	xc_vcpuinfo_t info;
-	int iter = 0;
+    int rc;
+    xen_instance_t *xen = xen_get_instance(vmi);
 
-	for (int i = 0; i < 2; i++) {
-		do {
-			iter++;
-			xen->libxcw.xc_vcpu_getinfo(xen->xchandle, xen->domainid, i, &info);
-		} while (!info.blocked && !info.running);
-	}
+    xc_dominfo_t dominfo;
+    xc_vcpuinfo_t info;
 
-	errprint("success afte %d iter\n", iter);
+    rc = xen->libxcw.xc_domain_getinfo(xen->xchandle, xen->domainid, 1, &dominfo);
 
+    if ( rc != 1 ) {
+        errprint("Failed to get domain info, rc=%d\n", rc);
+	return VMI_FAILURE;
+    }
+
+    int iter = 0;
+
+    for (int vcpu = 0; vcpu <= dominfo.max_vcpu_id; vcpu++) {
+        do {
+            iter++;
+            rc = xen->libxcw.xc_vcpu_getinfo(xen->xchandle, xen->domainid, vcpu, &info);
+
+	    if ( rc < 0 ) {
+                errprint("Failed to get vcpu info for vcpu=%d, rc=%d", vcpu, rc);
+	    }
+        } while (!info.blocked && !info.running && info.online);
+    }
+
+    errprint("success after %d iterations for total %d vCPUs\n", iter, dominfo.max_vcpu_id);
     return VMI_SUCCESS;
 }
 
